6b78fcc29d89646e07768591ad3257a96cf7df7b
Mehdi <mehdirochdi@MacBook-Pro-de-aicha.local>
2024-02-05 18:10:17 +0100

    added list product

--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -1,20 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
 
-export const FooterComponent = () => {
-    const currentYear = new Date().getFullYear()
-  return (
-  
-    <footer>
-    <Container>
-        <Row>
-            <Col className='text-center py-3'>
-                <p>Proshop &copy; {currentYear} ProShop</p>
-            </Col>
-        </Row>
-    </Container>
-    </footer>
-  
-  )
-}
+ const FooterComponent = () => {
+   const currentYear = new Date().getFullYear()
+   return (
+     <footer>
+       <Container>
+         <Row>
+           <Col className="text-center py-3">
+             <p>Proshop &copy; {currentYear} ProShop</p>
+           </Col>
+         </Row>
+       </Container>
+     </footer>
+   )
+ }
+export default FooterComponent
 
