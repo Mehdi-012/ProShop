c874f75b3e5e8dca2b8649a8bd1da701f5c3f011
Mehdi <mehdirochdi@MacBook-Pro-de-aicha.local>
2024-02-05 14:55:45 +0100

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './Component/Header'
+import './src/assets/styles/bootstrap.min.css';
+import './src/assets/styles/index.css';
 
 const App = () => {
   return (
-    <div>
-      <h1> Welcome to Proshop</h1>
-    </div>
+    <>
+    <Header /> 
+    <main className='py-3'>
+      <Container> 
+        <h1> Welcome to Proshop</h1>
+      </Container>
+    </main>
+    <Footer />
+    </>
+    
   )
 }
 
---
+++ frontend/src/Component/Footer.jsx
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+export const Footer = () => {
+    const currentYear = new Date().getFullYear()
+  return (
+  
+    <footer>
+    <Container>
+        <Row>
+            <Col className='text-center py-3'>
+                <p>Proshop &copy; {currentYear} ProShop</p>
+            </Col>
+        </Row>
+    </Container>
+    </footer>
+  
+  )
+}
+
