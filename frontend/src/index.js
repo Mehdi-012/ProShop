2f0850244b7579ad470a63fef81da215212fa802
Mehdi <mehdirochdi@MacBook-Pro-de-aicha.local>
2024-02-06 15:17:44 +0100

    Product details page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,12 +11,14 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />} >
+    <Route path='/' element={<App />} >
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
